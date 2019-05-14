b00bda0426ae0c10b831022e7b409a543e1fe9fc
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2019-05-13 21:09:30 -0300

    first commit

---
+++ src/main/java/br/univille/gr/gr/GrApplication.java
@@ -0,0 +1,13 @@
+package br.univille.gr.gr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/univille/gr/gr/GrApplicationTests.java
@@ -0,0 +1,16 @@
+package br.univille.gr.gr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GrApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
