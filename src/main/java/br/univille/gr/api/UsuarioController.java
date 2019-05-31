d5fa1d2fcbe9103f18f1802eb2945550d1ba4b48
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2019-05-30 21:02:41 -0300

    Foi

--- src/main/java/br/univille/gr/api/UsuarioController.java
+++ src/main/java/br/univille/gr/api/UsuarioController.java
@@ -1,12 +1,13 @@
-package br.univille.gr.controller;
+package br.univille.gr.api;
 
 import br.univille.gr.model.Usuario;
 import br.univille.gr.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -17,10 +18,16 @@ public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
 
+    @GetMapping
+    public ResponseEntity<List<Usuario>> listarUsuarios() {
+        List<Usuario> lista = usuarioService.getAll();
+        return new ResponseEntity<List<Usuario>>(lista, HttpStatus.OK);
+    }
+/*
     @GetMapping()
     public ModelAndView index() {
         List<Usuario> lista = usuarioService.getAll();
         return new ModelAndView("usuario/index", "usuarios", lista);
-    }
+    }*/
 
 }
