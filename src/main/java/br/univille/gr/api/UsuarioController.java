4837011eb463248c4585528a55e3aa4ad581a0a0
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2019-06-03 20:54:40 -0300

    Mais coisas

--- src/main/java/br/univille/gr/api/UsuarioController.java
+++ src/main/java/br/univille/gr/api/UsuarioController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -17,17 +20,23 @@ public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
-
+/*
     @GetMapping
     public ResponseEntity<List<Usuario>> listarUsuarios() {
         List<Usuario> lista = usuarioService.getAll();
         return new ResponseEntity<List<Usuario>>(lista, HttpStatus.OK);
-    }
-/*
+    }*/
+
+    @CrossOrigin(origins = "*")
     @GetMapping()
     public ModelAndView index() {
         List<Usuario> lista = usuarioService.getAll();
         return new ModelAndView("usuario/index", "usuarios", lista);
-    }*/
+    }
+
+    @GetMapping("/novo")
+    public ModelAndView createForm(@ModelAttribute Usuario usuario) {
+        return new ModelAndView("usuario/form");
+    }
 
 }
