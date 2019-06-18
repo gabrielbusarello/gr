7bb819b752a11cbe3afb8d170fb4bcd0a2ea0556
Gabriel Dezan Busarello <gabriel20053@gmail.com>
2019-06-17 21:33:52 -0300

    Faz o usuário controller

--- src/main/java/br/univille/gr/api/UsuarioController.java
+++ src/main/java/br/univille/gr/api/UsuarioController.java
@@ -5,14 +5,14 @@ import br.univille.gr.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("/usuario")
+@CrossOrigin(origins = "*")
 public class UsuarioController {
 
     @Autowired
